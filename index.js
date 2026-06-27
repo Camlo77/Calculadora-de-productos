9258aa7d643aabd2fe0121cb4b2c6d7f18d8a092
camlo <camilopasten.arancibia@gmail.com>
2026-06-27 15:43:09 -0400

    add: functions for buttons on javascript + and -

---
+++ index.js
@@ -0,0 +1,21 @@
+let precioSpan = document.querySelector(".precio-inicial");
+let precioTotal = document.querySelector(".valor-total")
+precioSpan.innerHTML = precio
+let precio = 400000
+
+let cantidad = document.querySelector(".cantidad")
+let agregar = document.querySelector("#btn-agregar")
+let restar = document.querySelector("#btn-restar")
+let contador = 0
+
+agregar.addEventListener('click', function(){
+    contador++
+    cantidad.innerHTML = contador
+    precioTotal.innerHTML = contador*precio
+});
+
+restar.addEventListener('click', function(){
+    contador--
+    cantidad.innerHTML = contador
+    precioTotal.innerHTML = contador*precio
+});
